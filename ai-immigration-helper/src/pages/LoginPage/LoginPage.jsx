b003547c8263ae9e3c393ca760269f485a30b997
DanySA22 <winache22@gmail.com>
2024-03-26 14:27:34 -0700

    Add style for Login and Signup pages components

--- ai-immigration-helper/src/pages/LoginPage/LoginPage.jsx
+++ ai-immigration-helper/src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {useState, useEffect} from 'react'  
 import {useNavigate} from 'react-router-dom' 
-
+import "./LoginPage.scss"
 
 
 function LoginPage () {
--- ai-immigration-helper/src/pages/SignupPage/SignupPage.jsx
+++ ai-immigration-helper/src/pages/SignupPage/SignupPage.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {useState, useEffect} from 'react'  
 import {useNavigate} from 'react-router-dom' 
-
+import "./SignupPage.scss"
 
 
 function SignupPage () {
