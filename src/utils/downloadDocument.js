825b7448267a66060535aa96fcdbb15992d9df63
DanySA22 <winache22@gmail.com>
2024-03-29 18:22:00 -0700

    Add babel dependency and add noopener on Footer links

--- src/utils/downloadDocument.js
+++ src/utils/downloadDocument.js
@@ -4,65 +4,6 @@ import html2canvas from 'html2canvas'
 
 export const downloadToPDF = (contentRef) =>{
     try {
-        // const newPDF = async () => {
-        // const input = contentRef.current
-        // if(!input) throw new Error('Invalid content reference')
-        
-        // const originalStyle = contentRef.current.getAttribute('style')
-        // contentRef.current.style.height = 'auto'
-        // contentRef.current.style.overflow = 'visible'
-
-        // const scale = 2
-
-        // const canvas = await html2canvas(input, {
-        // scrollY: -window.scrollY,
-        // scale: scale
-        // })
-
-        // contentRef.current.setAttribute('style', originalStyle)
-        // const imgPdf = canvas.toDataURL('image/png')
-        // const documentPDF = new jsPDF('p', 'mm', 'a4')
-
-        // const margin = {
-        //     top: 10,
-        //     left: 10,
-        //     right: 10,
-        //     bottom: 10,
-        // }
-        
-        //     const pageWidth = documentPDF.internal.pageSize.getWidth() - margin.left - margin.right;
-        //     const pageHeight = documentPDF.internal.pageSize.getHeight() - margin.top - margin.bottom;
-
-        //     let imgWidth = (canvas.width / scale) * 0.264583; // Convert pixels to mm (1px = 0.264583mm)
-        //     let imgHeight = (canvas.height / scale) * 0.264583; // Scale and convert to mm
-        //     // Scale image to fit within page dimensions while maintaining aspect ratio
-            
-        //     const ratio = pageWidth / imgWidth;
-        //     imgWidth = pageWidth; // Fit to page width
-        //     imgHeight *= ratio; // Adjust height to maintain aspect ratio
-            
-
-        //     // if (imgHeight > pageHeight) {
-        //     //     const newRatio = pageHeight / imgHeight;
-        //     //     imgHeight = pageHeight; // Fit to page height
-        //     //     // imgWidth *= newRatio; // Adjust width to maintain aspect ratio
-        //     // }
-
-           
-        //     let heightLeft = imgHeight;
-
-        //     // Split image across pages if needed
-        //     while (heightLeft > 0) {
-        //         let pageHeightToUse = heightLeft > pageHeight ? pageHeight : heightLeft;
-        //         documentPDF.addImage(imgPdf, 'PNG', margin.left, margin.top, imgWidth, pageHeightToUse);
-
-        //         heightLeft -= pageHeight;
-        //         if (heightLeft > 0) {
-        //             documentPDF.addPage();
-        //             margin.top = 10; // Reset Y position for the new page
-        //         }
-        //     }
-
         const newPDF = async () => {
             const input = contentRef.current;
             if (!input) throw new Error('Invalid content reference');
